9b9581746157bdeef62fe6dd907dab1f2db1af6a
Jiajie Chen <c@jia.je>
2023-06-11 19:04:54 +0800

    Cleanup unused code

--- kernel/include/unistd.h
+++ kernel/include/unistd.h
@@ -19,9 +19,6 @@
 #define PTECODE_SIZE    ((RAM1BASE - RAM0UBASE) / 0x1000 * 4)
 #define PTESTACK_SIZE   ((RAM1KBASE - RAM1BASE) / 0x1000 * 4)
 
-
-#define KSEG2PAGE0  0xC0000000      // kseg2中页表的第一页位置
-
 /* for EntryLo0-1   */
 #define ELO_VALIDB  0
 #define ELO_VALIDF  ((1) << 0)
@@ -58,46 +55,6 @@
 #define SYS_getcwd          121
 #define SYS_getdirentry     128
 #define SYS_dup             130
-
-/* fetch program from serial bus */
-#define SYS_fetchrun		241	 // a prime number :)
-
-#define SYS_redraw_console		242
-
-/* OLNY FOR LAB6 */
-#define SYS_lab6_set_priority 255
-
-/* SYS_fork flags */
-#define CLONE_VM            0x00000100  // set if VM shared between processes
-#define CLONE_THREAD        0x00000200  // thread group
-#define CLONE_FS            0x00000800  // set if shared between processes
-
-/* VFS flags */
-// flags for open: choose one of these
-#define O_RDONLY            0           // open for reading only
-#define O_WRONLY            1           // open for writing only
-#define O_RDWR              2           // open for reading and writing
-// then or in any of these:
-#define O_CREAT             0x00000004  // create file if it does not exist
-#define O_EXCL              0x00000008  // error if O_CREAT and the file exists
-#define O_TRUNC             0x00000010  // truncate file upon open
-#define O_APPEND            0x00000020  // append on each write
-// additonal related definition
-#define O_ACCMODE           3           // mask for O_RDONLY / O_WRONLY / O_RDWR
-
-#define NO_FD               -0x9527     // invalid fd
-
-/* lseek codes */
-#define LSEEK_SET           0           // seek relative to beginning of file
-#define LSEEK_CUR           1           // seek relative to current position in file
-#define LSEEK_END           2           // seek relative to end of file
-
-#define FS_MAX_DNAME_LEN    31
-#define FS_MAX_FNAME_LEN    255
-#define FS_MAX_FPATH_LEN    4095
-
-#define EXEC_MAX_ARG_NUM    32
-#define EXEC_MAX_ARG_LEN    4095
 
 #endif /* !__LIBS_UNISTD_H__ */
 
